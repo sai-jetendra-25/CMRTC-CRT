c5bef2b753d1634fe5ccc9861d8611dee48e3a3c
Sai Jetendra <saijithendra807@gmail.com>
2026-01-27 12:07:27 +0530

    Simple ZeroDivision Excep

---
+++ ExceptionHandling/ArthimeticExcep.java
@@ -0,0 +1,17 @@
+package ExceptionHandling;
+public class ArthimeticException {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 0;
+
+        try {
+            System.out.println(a / b);
+        }
+        catch (ArithmeticException ob) {
+            System.out.println("b cannot be zero");
+        }
+    }
+}
+    
+
